f2fc0849ef2c0cda5217734cca80fb0288f18973
BloomingLight <bloominglightart@gmail.com>
2018-05-01 12:27:20 -0700

    Starting edit of MM.js

---
+++ js/MainMenu.js
@@ -0,0 +1,15 @@
+Game.MainMenu = function(game) {
+    
+    
+};
+
+Game.MainMenu.prototype = {
+    create:function() {
+        
+    }, 
+    update:function() {
+        
+        
+    }
+    
+};
