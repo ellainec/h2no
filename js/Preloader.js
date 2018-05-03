26417015aa9e728292d669766e389fa25cbe3926
Kevin Vizconde <kevin.vizconde@gmail.com>
2018-05-03 16:50:25 -0700

    Updated Sprinkler Test

--- js/Preloader.js
+++ js/Preloader.js
@@ -26,6 +26,7 @@ Game.Preloader.prototype = {
         this.load.image('logo', '../assets/logo.png');
         
         this.load.image('sprinkler', '../assets/sprinkler.png');
+        this.load.image('diamond', '../assets/diamond.png');
         
     },
     
