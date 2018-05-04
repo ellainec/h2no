ad0a6426b1b1b4dcb36b6b93c57805c99b923a8b
ellainec <chan.ellaine@gmail.com>
2018-05-04 16:36:19 -0700

    changed file hiearchy

--- js/Preloader.js
+++ js/Preloader.js
@@ -11,25 +11,25 @@ Game.Preloader.prototype = {
         
         this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
         // load player sprite
-        this.load.image('WaterBot', '../assets/WaterBot.png');
+        this.load.image('WaterBot', 'assets/WaterBot.png');
         // load map
-        this.load.tilemap('map', '../assets/tilemaps/maps/H2NO.csv');
+        this.load.tilemap('map', 'assets/tilemaps/maps/H2NO.csv');
         
         
         // load tiles
-        this.load.image('tileset', '../assets/tilemaps/tiles/TileSpriteMap.png');
+        this.load.image('tileset', 'assets/tilemaps/tiles/TileSpriteMap.png');
         
-        this.load.image('buttonLeft', '../assets/buttons/arrowLeft.png');
-        this.load.image('buttonJump', '../assets/buttons/arrowUp.png');
-        this.load.image('buttonRight', '../assets/buttons/arrowRight.png');
+        this.load.image('buttonLeft', 'assets/buttons/arrowLeft.png');
+        this.load.image('buttonJump', 'assets/buttons/arrowUp.png');
+        this.load.image('buttonRight', 'assets/buttons/arrowRight.png');
         
-        this.load.image('logo', '../assets/logo.png');
+        this.load.image('logo', 'assets/logo.png');
 
-        this.load.spritesheet('baddie', '../assets/baddie.png', 32, 32, 4);
-        this.load.spritesheet('gamepad', '../assets/gamepad/gamepad_spritesheet.png', 100, 100);
+        this.load.spritesheet('baddie', 'assets/baddie.png', 32, 32, 4);
+        this.load.spritesheet('gamepad', 'assets/gamepad/gamepad_spritesheet.png', 100, 100);
         
-        this.load.image('sprinkler', '../assets/sprinkler.png');
-        this.load.image('diamond', '../assets/diamond.png');
+        this.load.image('sprinkler', 'assets/sprinkler.png');
+        this.load.image('diamond', 'assets/diamond.png');
         
     },
     
