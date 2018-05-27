515c4239544d201f2f6980e3f1230822e48f21e0
ellainec <chan.ellaine@gmail.com>
2018-05-27 15:42:20 -0700

    test

---
+++ db/test.php
@@ -0,0 +1,25 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "h2no";
+
+
+    try {
+        //create connection
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        //set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $sql2="SELECT * FROM alltime ORDER BY score DESC";
+        $statement2 = $conn->query($sql2);
+        $data =  $statement2->fetchAll(PDO::FETCH_ASSOC));
+                echo $data;
+
+     }
+
+    catch(PDOException $e)
+        {
+        echo "failed ". $e->getMessage();
+        }
+?>
